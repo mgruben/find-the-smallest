c30339af91891ff55bdf9318344c37d55b646d81
mgruben <GrubenM@gmail.com>
2016-09-24 22:57:31 -0500

    Implement smallest as wrapper

--- Solution.cpp
+++ Solution.cpp
@@ -4,34 +4,6 @@
 
 using namespace std;
 
-vector<long long> vectorize(long long n) {
-    vector<long long> rev_ans;
-    while (n) {
-        rev_ans.push_back(n % 10);
-        n /= 10;
-    }
-    vector<long long> ans;
-    while (!rev_ans.empty()) {
-        ans.push_back(rev_ans.back());
-        rev_ans.pop_back();
-    }
-    return ans;
-}
-
-long long numberize(vector<long long> v) {
-    vector<long long> rev_v;
-    while (!v.empty()) {
-        rev_v.push_back(v.back());
-        v.pop_back();
-    }
-    long long ans = 0;
-    while (!rev_v.empty()) {
-        ans *= 10;
-        ans += rev_v.back();
-        rev_v.pop_back();
-    }
-    return ans;
-}
 
 string toString(vector<long long> v) {
     string ans = "{";
@@ -43,7 +15,36 @@ string toString(vector<long long> v) {
 }
 
 class ToSmallest {
-public:
+private:
+    static vector<long long> vectorize(long long n) {
+        vector<long long> rev_ans;
+        while (n) {
+            rev_ans.push_back(n % 10);
+            n /= 10;
+        }
+        vector<long long> ans;
+        while (!rev_ans.empty()) {
+            ans.push_back(rev_ans.back());
+            rev_ans.pop_back();
+        }
+        return ans;
+    }
+
+    static long long numberize(vector<long long> v) {
+        vector<long long> rev_v;
+        while (!v.empty()) {
+            rev_v.push_back(v.back());
+            v.pop_back();
+        }
+        long long ans = 0;
+        while (!rev_v.empty()) {
+            ans *= 10;
+            ans += rev_v.back();
+            rev_v.pop_back();
+        }
+        return ans;
+    }
+    
     static vector<long long> sendBackward(long long n) {
         vector<long long> v = vectorize(n);
         
@@ -86,17 +87,27 @@ public:
         vector<long long> ans = {numberize(v_unsorted), takeFrom, insertAt};
         return ans;
     }
-    static vector<long long> smallest(long long n);
+
+public:
+    static vector<long long> smallest(long long n) {
+        vector<long long> backward = sendBackward(n);
+        vector<long long> forward = sendForward(n);
+        if (backward[0] < forward[0]) return backward;
+        else if (forward[0] < backward[0]) return forward;
+        else if (forward[1] < backward[1]) return forward;
+        else if (backward[1] < forward[1]) return backward;
+        else return forward;
+    }
 };
 
 int main() {
     ToSmallest t;
-    cout << toString(t.sendForward(261235)) << endl;
-    cout << toString(t.sendBackward(209917)) << endl;
-    cout << toString(t.sendForward(285365)) << endl;
-    cout << toString(t.sendForward(269045)) << endl;
-    cout << toString(t.sendForward(296837)) << endl;
-    cout << toString(t.sendBackward(1000000)) << endl;
+    cout << toString(t.smallest(261235)) << endl;
+    cout << toString(t.smallest(209917)) << endl;
+    cout << toString(t.smallest(285365)) << endl;
+    cout << toString(t.smallest(269045)) << endl;
+    cout << toString(t.smallest(296837)) << endl;
+    cout << toString(t.smallest(1000000)) << endl;
 
     return 0;
 }
